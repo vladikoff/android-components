81d466a95d7b3cb4b28a65c7ac0ef0ac9fd2db36
vladikoff <vlad.filippov@gmail.com>
2019-01-24 23:36:58 -0500

    init

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -28,6 +28,7 @@ object Versions {
 
     const val sentry = "1.7.14"
     const val okhttp = "3.12.1"
+    const val zxing = "3.3.0"
 
     const val mozilla_appservices = "0.15.0"
     const val servo = "0.0.1.20181017.aa95911"
@@ -77,4 +78,5 @@ object Dependencies {
 
     const val thirdparty_okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
     const val thirdparty_sentry = "io.sentry:sentry-android:${Versions.sentry}"
+    const val thirdparty_zxing = "com.google.zxing:core:${Versions.zxing}"
 }
---
+++ components/lib/qr/src/test/java/mozilla/components/lib/crash/QrTest.kt
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.components.lib.qr
+
+import android.content.Intent
+import org.junit.Assert.assertArrayEquals
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.lang.RuntimeException
+
+@RunWith(RobolectricTestRunner::class)
+class QrTest {
+    @Test
+    fun `can test`() {
+
+    }
+
+}
